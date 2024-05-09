f4842f645f337a56691ff5b384ec5f566acae634
icantfunction <opendavidfunction@gmail.com>
2024-05-09 11:09:58 -0400

    Maybe 10% Done

---
+++ script.js
@@ -0,0 +1,9 @@
+let faith = 0;
+let spirit = 100;
+let honey = 50;
+let currentWeapon = 0;
+let fighting;
+let demonHealth;
+let inventory = ['stick']
+
+const button1 = document.querySelector('#button1')
