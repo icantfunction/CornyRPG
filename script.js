6135929029b5df71a81434f521c498629d5c72f8
icantfunction <opendavidfunction@gmail.com>
2024-05-10 00:12:56 -0400

    17% done now

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ let demonHealth;
 let inventory = ['stick']
 
 const button1 = document.querySelector('#button1')
+const button2 = document.querySelector('#button2')
+const button3 = document.querySelector('#button3')
+
+const text = document.querySelector('#text')
+const faithText = document.querySelector('#faith')
+const spiritText = document.querySelector('#spirit')
+const honeyText = document.querySelector('#honey')
+const demonStats = document.querySelector('#demonStats')
